b89792e38fbc5036f8e535afd62d45b86affcf30
codebudy5247 <a.shekhar5247@gmail.com>
2024-02-02 18:36:29 +0530

    add validation

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,13 +2,22 @@ import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
 import { generateAccountNumber } from "@/lib/utils";
+import * as z from "zod";
 
+const UserSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have than 8 characters"),
+});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
 
-    const { email, password, username } = body;
+    const { email, password, username } = UserSchema.parse(body);
     const existingUserByEmail = await db.user.findUnique({
       where: { email: email },
     });
@@ -26,18 +35,20 @@ export async function POST(req: Request) {
       data: {
         email,
         username,
-        password:hashedPassword,
+        password: hashedPassword,
         accountNumber: uniqueAccountNumber,
       },
     });
+    const { password: newUserPassword, ...rest } = newUser;
     return NextResponse.json(
       {
-        user: newUser,
+        user: rest,
         message: "User created successfully",
       },
       { status: 201 }
     );
   } catch (error) {
     console.log(error);
+    NextResponse.json({ message: "Something went wrong" }, { status: 500 });
   }
 }
