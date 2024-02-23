45564d0fe38cd07b857948b6bb54befef046ff30
codebudy5247 <a.shekhar5247@gmail.com>
2024-02-23 19:10:04 +0530

    :hammer:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
+import { authOptions } from "@/app/auth";
 import { getUserByEmail,getUserTxs } from "@/lib/data";
 import Navbar from "@/components/Navbar";
 import Container from "@/components/ui/Container";
